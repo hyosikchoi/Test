2ea83624c440333543cc69ec4477432c12a1e8be
hyosikchoi <huchoi24@naver.com>
2025-01-20 01:52:46 +0900

    init

---
+++ app/src/main/java/com/hyosik/submoduleex/Test.kt
@@ -0,0 +1,13 @@
+package com.hyosik.submoduleex
+
+import android.content.Context
+import android.widget.Toast
+
+object Test {
+
+    @JvmStatic
+    fun test(context: Context, text: String) {
+        Toast.makeText(context, text, Toast.LENGTH_SHORT).show()
+    }
+
+}
